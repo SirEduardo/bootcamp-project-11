00c359d96bd8e142eff18ca932e52376e18ce63b
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-02 12:51:30 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,17 +5,32 @@ const Header = () => {
     <header className="h-20 bg-yellow-400 flex items-center justify-center">
       <nav className=" w-full flex justify-around list-none font-semibold">
         <li>
-          <NavLink to="/" className={({isActive}) => isActive ? "border-b-2 border-black" : ""}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "border-b-2 border-black" : ""
+            }
+          >
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/search" className={({isActive}) => isActive ? "border-b-2 border-black" : ""}>
+          <NavLink
+            to="/search"
+            className={({ isActive }) =>
+              isActive ? "border-b-2 border-black" : ""
+            }
+          >
             Search Pokemon
           </NavLink>
         </li>
         <li>
-          <NavLink to="/about" className={({isActive}) => isActive ? "border-b-2 border-black" : ""}>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? "border-b-2 border-black" : ""
+            }
+          >
             About
           </NavLink>
         </li>
