9e93fa45029f7430219b2fea3c0c21012679fb89
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-02 12:51:21 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header"
 import Home from "./pages/Home/Home"
 import Search from "./pages/Search/Search"
 import About from "./pages/About/About"
+import PokemonById from "./pages/PokemonById/PokemonById"
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/search" element={<Search />} />
       <Route path="/about" element={<About />} />
+      <Route path="/pokemon/:id" element={<PokemonById />} />
       </Routes>
       </div>
   )
