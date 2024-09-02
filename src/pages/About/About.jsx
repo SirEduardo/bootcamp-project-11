c90eea911fab2be24dbfcf2313c9afc79224be8f
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-02 12:51:36 +0200

    Update About.jsx

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,12 @@
 
 const About = () => {
-
+    return (
+        <main className="min-h-screen flex flex-col items-center justify-center gap-3 bg-lime-400">
+            <div>
+                <p>Pokedex super mega guay de todas las generaciones</p>
+            </div>
+        </main>
+    )
 }
 
 export default About
