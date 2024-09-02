fa98be69d3692af12076b6ece7ae93ada85a8d64
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-02 10:32:10 +0200

    create-loading

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="flex items-center gap-5">
+        <div className="border-4 border-customWhite border-solid border-t-white rounded-full w-10 h-10 animate-spin"></div>
+        <p>Cargando...</p>
+        </div>
+    )
+}
+export default Loading
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customWhite: 'rgba(255, 255, 255, 0.3)',
+      },
+      animation: {
+        spin: "spin 1s linear infinite"
+      }
+    },
   },
   plugins: [],
 }
