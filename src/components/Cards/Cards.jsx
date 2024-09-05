65982d89b34ca1650ee00b44096be520bb5328f0
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-05 13:36:39 +0200

    Update Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,9 @@
 const Cards = ({ name, id, img, types }) => {
   return (
     <div
-      className="bg-blue-500 shadow-lg shadow-black mt-32 w-72 h-80 rounded-2xl 
-        flex flex-col gap-14 items-center relative overflow-hidden duration-200
-        hover:cursor-pointer hover:-rotate-6 hover:-translate-y-5 "
+      className="bg-blue-500 shadow-lg shadow-black w-72 h-80 rounded-2xl 
+        flex flex-col items-center relative overflow-hidden duration-200
+        hover:cursor-pointer hover:-rotate-6 hover:-translate-y-10"
     >
       <p className="absolute top-2 left-4 text-2xl text-white">#{id}</p>
       <p className="bg-red-500 py-2 w-2/4 absolute top-0 right-0 text-center text-white text-xl">
