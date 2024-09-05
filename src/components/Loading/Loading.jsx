e80e0a88b7ec6d5cd9b96fe32cc3e6eea0862a31
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-05 13:36:44 +0200

    Update Loading.jsx

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="flex items-center gap-5">
+    <div className="flex items-center gap-5 max-lg:gap-2">
       <div className="border-4 border-customWhite border-solid border-t-white rounded-full w-10 h-10 animate-spin"></div>
       <p>Cargando...</p>
     </div>
